53ac71e125ff6e4f69f5f7cec8f3ba2b56532489
Magmeta <125174106+Des-Magmeta@users.noreply.github.com>
2025-08-01 14:21:03 +0800

    Create merge.py

---
+++ Sources/scripts/merge.py
@@ -0,0 +1,38 @@
+import glob
+import shutil
+from pathlib import Path
+import os
+import ufoLib2
+from ufomerge import merge_ufos
+
+SOURCE = Path("sources")
+EXPORT = Path("sources/build")
+EXPORT.mkdir(exist_ok=True)
+
+for file in SOURCE.glob("*.ufo"):
+    # We keep a single copy of the common Hanzi data, and merge it into the Latins as necessary
+    coreUFO = ufoLib2.Font.open(file)
+    if "Regular" in str(file):
+        commonUFO = ufoLib2.Font.open("sources/temp/LXGWWenKaiKR_common-Regular.ufo")
+    elif "Medium" in str(file):
+        commonUFO = ufoLib2.Font.open("sources/temp/LXGWWenKaiKR_common-Medium.ufo")
+    elif "Bold" in str(file):
+        commonUFO = ufoLib2.Font.open("sources/temp/LXGWWenKaiKR_common-Bold.ufo")
+    else:
+        commonUFO = ufoLib2.Font.open("sources/temp/LXGWWenKaiKR_common-Light.ufo")
+
+    print(f"Merging {file.name}")
+
+    merge_ufos(
+        coreUFO,
+        commonUFO,
+        layout_handling="ignore",
+        existing_handling="skip",
+    )
+
+    # Override the feature code for consistency
+    f = open("sources/features.fea", "r")
+    features = f.read()
+
+    coreUFO.features.text = features
+    coreUFO.save(EXPORT / str(file.name), overwrite=True, validate=False)
