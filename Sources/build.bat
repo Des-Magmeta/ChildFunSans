799d4008bf1862ca76ef43fe0e45dbacb62845e9
Magmeta <125174106+Des-Magmeta@users.noreply.github.com>
2025-08-01 13:24:43 +0800

    Create build.bat

---
+++ Sources/build.bat
@@ -0,0 +1,2 @@
+fontmake -u ChildFunSansBeta-Regular.ufoz --keep-overlaps --keep-direction --no-generate-GDEF --no-production-names -o ttf --output-dir ../fonts/TTF
+fontmake -u ChildFunSansBetaZHS-Regular.ufoz --keep-overlaps --keep-direction --no-generate-GDEF --no-production-names -o ttf --output-dir ../fonts/TTF
