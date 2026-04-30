8191344a758d28744424b2bd82380327d917b619
Nakadai Magmeta <125174106+Des-Magmeta@users.noreply.github.com>
2026-05-01 00:03:24 +0800

    Remove redundant build command for ChildFunSans-ZHS.ttf

--- Sources/build.bat
+++ Sources/build.bat
@@ -1,2 +1,1 @@
 fontmake -u ChildFunSans-Regular.ufoz --keep-overlaps --keep-direction --no-generate-GDEF --no-production-names -o ttf --output-dir ../fonts/TTF
-fontmake -u ChildFunSansZHS-Regular.ufoz --keep-overlaps --keep-direction --no-generate-GDEF --no-production-names -o ttf --output-dir ../fonts/TTF
